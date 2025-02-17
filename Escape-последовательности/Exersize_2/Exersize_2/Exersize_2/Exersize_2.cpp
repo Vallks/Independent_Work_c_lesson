f251fdda47cecbf1e79f046bef0e3e0eb915c738
Валентин Командиров <komandirov.v@mail.ru>
2025-02-17 23:54:03 +0300

    Самостоятельное изучение 17.02.2025

---
+++ Escape-последовательности/Exersize_2/Exersize_2/Exersize_2/Exersize_2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    cout << "I study programming language C\\C++\n";
+    cout << "\Я изучаю язык программирования \"C++\"";
+
+    return 0;
+}
---
+++ Exersize_1/Exercise_2/Exercise_2/Exercise_2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    cout << "Отчего работать дяди\n";
+    cout << "Стали лучше поутру?\n";
+    cout << "Просто им администратор\n";
+    cout << "Отключил vkontakte.ru!";
+
+    return 0;
+}
