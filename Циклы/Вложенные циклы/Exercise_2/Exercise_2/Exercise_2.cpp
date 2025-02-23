23facd1a863b65dfd9a2e8525c61db5b8cb34447
Валентин Командиров <komandirov.v@mail.ru>
2025-02-24 01:12:27 +0300

    add exercise

---
+++ Циклы/Вложенные циклы/Exercise_2/Exercise_2/Exercise_2.cpp
@@ -0,0 +1,33 @@
+﻿/*Пользователь вводит высоту треугольника. Вывести на консоль треугольник следующего вида:*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, len;
+
+	len = 0;
+
+	cin >> x;
+
+	for (int i = 1; i <= x; i++)
+	{
+		for (int j = 1; j <= x; j++)
+		{
+			if (x - len <= j)
+			{
+				cout << '*';
+			}
+			else
+			{
+				cout << ' ';
+			}
+		}
+		
+		cout << endl;
+		len++;
+	}
+
+	return 0;
+}
---
+++ Циклы/Вложенные циклы/Exerxise_3/Exerxise_3/Exerxise_3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int c;
+
+	cin >> c;
+	bool isFirst = true;
+
+	for (int j = c; j >= 0; j--) {
+		
+		if (isFirst == false && j > c / 2)
+		{
+			cout << "\n";
+		}
+		else
+		{
+			isFirst = false;
+		}
+
+		for (int i = 1; i <= j; i++) {
+		
+			if (i <= (c - j))
+			{
+				cout << " ";
+			}
+			else
+			{
+				cout << "*";
+			}
+		}
+	}
+	
+	return 0;
+}
