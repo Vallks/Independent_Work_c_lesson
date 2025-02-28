11c3d1612e4012ee9eae713b8451642a05742861
Валентин Командиров <komandirov.v@mail.ru>
2025-02-28 23:48:03 +0300

    Добавил упражнений

---
+++ Сортировки/Exercise_3/Exercise_3/Exercise_3.cpp
@@ -0,0 +1,51 @@
+﻿/*Ввести десять целых чисел в одномерный массив. Предполагается, что пользователь вводит упорядоченные по возрастанию данные. 
+Также ввести число для поиска. Все данные разделяются пробелами.
+
+Используя бинарный поиск, найти индекс искомого числа. Вывести этот индекс или слово "no", если данный элемент отсутствует в массиве.*/
+#include <iostream>
+
+int main()
+{
+    const int SIZE = 10;
+	int ar[SIZE];
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		std::cin >> ar[i];
+	}
+
+	int poisk, low = 0, up = SIZE - 1, indx = -1;
+
+	std::cin >> poisk;
+
+	do
+	{
+		int midl = (low + up) / 2;
+		if (poisk == ar[midl])
+		{
+			indx = midl;
+			break;
+		}
+		if (poisk > ar[midl])
+		{
+			low = midl + 1;
+		}
+		if (poisk < ar[midl])
+		{
+			up = midl - 1;
+		}
+	} while (low <= up);
+
+	if (indx == -1)
+	{
+		std::cout << "no";
+	}
+	else
+	{
+		std::cout << indx;
+	}
+
+	return 0;
+}
+
+
